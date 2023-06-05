a6d7bb368a676af7accd24b61c12c3ad8a31ac00
Andrey <andrey.udeneew@mail.ru>
2023-06-05 05:43:54 +0300

    v0.2 Frame-difference + saving with timestamp were added

--- GxSingleCamMono.py
+++ GxSingleCamMono.py
@@ -9,12 +9,35 @@ from matplotlib import pyplot as plt
 from tkinter import *
 from tkinter import ttk, Tk, Label, Button, Text, Radiobutton
 from tkinter import filedialog
-from tkinter.filedialog import *
+import numba as nb
+
+# using datetime module
+import datetime
+ct = datetime.datetime.now()
+ts = ct.timestamp()
+print("timestamp:-", ts)
+from datetime import datetime
+date_time = datetime.fromtimestamp(ts)
+str_date_time = date_time.strftime("%Y-%m-%d_%H_%M_%S")
+filename = str_date_time + ".png"
+print(filename)
+
+# ct stores current time
+
+# print("Current timestamp", str_date_time)
 
 def selectOutputDir():
     text1.delete(1.0, END)
     outputDir = filedialog.askdirectory(parent=window)
     text1.insert(INSERT, outputDir)
+
+# @nb.njit
+def sum_gt_nb(arr):
+    arr = arr.ravel()
+    result = 0
+    for x in arr:
+        result += x
+    return result
 
 def main():
     # print the demo information
@@ -46,7 +69,7 @@ def main():
     cam.TriggerMode.set(gx.GxSwitchEntry.OFF)
 
     # set exposure
-    cam.ExposureTime.set(40)
+    cam.ExposureTime.set(40000)
 
     # set gain
     cam.Gain.set(24.0)
@@ -59,29 +82,57 @@ def main():
     # for i in range(num):
     while (True):
         # get raw image
-        raw_image = cam.data_stream[0].get_image()
-        if raw_image is None:
-            print("Getting image failed.")
-            continue
+        raw_image1 = cam.data_stream[0].get_image()
+        frame1 = raw_image1.get_numpy_array()
+        raw_image2 = cam.data_stream[0].get_image()
+        frame2 = raw_image2.get_numpy_array()
+        # if raw_image is None:
+        #     print("Getting image failed.")
+        #     continue
 
         # create numpy array with data from raw image
-        numpy_image = raw_image.get_numpy_array()
-        if numpy_image is None:
-            continue
+        # numpy_image = raw_image.get_numpy_array()
+        # if numpy_image is None:
+        #     continue
+
+        sum1 = sum_gt_nb(frame1[1:100])
+        sum2 = sum_gt_nb(frame1[1:100])
+        #
+        if sum1 > sum2:
+            difframe = frame1 - frame2
+        else:
+            difframe = frame2 - frame1
 
         # show acquired image
-        pillow_im = img.fromarray(numpy_image, 'L')
+        pillow_im = img.fromarray(difframe, 'L')
         cv_im = np.array(pillow_im)
         plt.cla()
-        cv2.imshow('Preview', cv_im)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            import datetime
+            ct = datetime.datetime.now()
+            ts = ct.timestamp()
+            print("timestamp:-", ts)
+            from datetime import datetime
+            date_time = datetime.fromtimestamp(ts)
+            str_date_time = date_time.strftime("%Y-%m-%d_%H_%M_%S")
+            filename = text1.get("1.0", 'end-1c') + '/' + str_date_time + ".png"
+            print(filename)
+            cv2.imwrite(filename, cv_im)
+            continue
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+
+        cv2.imshow('Preview', cv_im)
         # img.show()
 
         # print height, width, and frame ID of the acquisition image
-        print("Frame ID: %d   Height: %d   Width: %d"
-              % (raw_image.get_frame_id(), raw_image.get_height(), raw_image.get_width()))
+        # print("Frame ID: %d   Height: %d   Width: %d"
+        #       % (raw_image1.get_frame_id(), raw_image1.get_height(), raw_image1.get_width()))
+        print(raw_image1.get_frame_id())
 
     # stop data acquisition
     cam.stream_off()
@@ -94,31 +145,14 @@ if __name__ == "__main__":
     window.geometry('1350x250')
     window.title("imageLinePlotter")
 
-    # lbl0 = Label(window, text="Выбор директории выходного файла")
-    # lbl0.grid(column=0, row=0)
-    # lbl1 = Label(window, text="Output Dir")
-    # lbl1.grid(column=0, row=0)
-    # lbl2 = Label(window, text="Y_line")
-    # lbl2.grid(column=0, row=1)
-    lbl3 = Label(window, text="Press q to stop video")
-    lbl3.grid(column=8, row=0)
+    lbl1 = Label(window, text="Press q to stop video")
+    lbl1.grid(column=8, row=0)
+    lbl2 = Label(window, text="Press s to save image")
+    lbl2.grid(column=8, row=1)
 
     text1 = Text(width=70, height=1)  # Output DIR
     text1.grid(column=1, row=0, sticky=W)
-    # text2 = Text(width=70, height=1)  # Background
-    # text2.grid(column=1, row=1, sticky=W)
-    # text3 = Text(width=70, height=1)  # Base (white field)
-    # text3.grid(column=1, row=2, sticky=W)
-    # text4 = Text(width=70, height=1)  # Output DIR
-    # text4.grid(column=1, row=4, sticky=W)
-    # text5 = Text(width=6, height=1)  # Coordinate
-    # text5.grid(column=5, row=0, sticky=W)
-    # text6 = Text(width=6, height=1)
-    # text6.grid(column=7, row=0, sticky=W)  # Status
-    # text7 = Text(width=70, height=1)  # Output DIR
-    # text7.grid(column=1, row=3, sticky=W)
-    # text8 = Text(width=70, height=1)  # Output DIR
-    # text8.grid(column=1, row=5, sticky=W)
+
     # text0.pack()
 
     btn1 = Button(window, text="Select Output Dir", command=selectOutputDir)
@@ -129,28 +163,6 @@ if __name__ == "__main__":
     btn3.grid(column=0, row=2, sticky=W)
     btn4 = Button(window, text="Save")
     btn4.grid(column=0, row=4, sticky=W)
-    # btn5 = Button(window, text="Plot easy!", command=plotEasy)
-    # btn5.grid(column=6, row=0, sticky=W)
-    # btn6 = Button(window, text="[(im/base)adjusted - (BG/baseBG)adjusted] + (BG/baseBG)adjusted",
-    #               command=plot_normalized_adjusted_4_overlay)
-    # btn6.grid(column=2, row=2, sticky=W)
-    # btn7 = Button(window, text="(im - BG) + (BG)green", command=plot_like_Eimar)
-    # btn7.grid(column=2, row=3, sticky=W)
-    # btn8 = Button(window, text="[(im/base)adjusted - (BG/baseBG)adjusted]adjusted",
-    #               command=plot_normalized_adjusted)
-    # btn8.grid(column=2, row=4, sticky=W)
-    # btn9 = Button(window, text="Select Base BG", command=WhiteFieldOpen_BG)
-    # btn9.grid(column=0, row=3, sticky=W)
-    # btn10 = Button(window, text="Select multiple", command=plotMultiple)
-    # btn10.grid(column=0, row=5, sticky=W)
-    # btn11 = Button(window, text="imadjust", command=im_adjust)
-    # btn11.grid(column=2, row=5, sticky=W)
-    # btn12 = Button(window, text="R/G", command=R2G)
-    # btn12.grid(column=2, row=6, sticky=W)
-    # btn13 = Button(window, text="R-G", command=R_G)
-    # btn13.grid(column=2, row=7, sticky=W)
-    # btn14 = Button(window, text="R/G vs R-G", command=comparison)
-    # btn14.grid(column=2, row=8, sticky=W)
 
     LED = BooleanVar()
     rb0 = Radiobutton(text="Violet", variable=LED, value=0)
